d10f04bed2eec008df305c39a2d1d0344953479b
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2021-07-15 23:21:38 -0300

    Added spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import * as M from 'materialize-css';
+
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('mobile') sideNav?: ElementRef;
+
   title = 'roubank-app';
+
+  ngAfterViewInit(): void {
+    M.Sidenav.init(this.sideNav?.nativeElement);
+  }
 }
