97f78a10ab4c72d392b8527a0c2826c191cc33ad
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2021-07-21 23:22:43 -0300

    Adicionado exemplo do routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { LandPageComponent } from './land-page/land-page.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 
-const routes: Routes = [{ path: '', component: LandPageComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: 'land-page', pathMatch: 'full' },
+  { path: 'land-page', component: LandPageComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  //imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
 import { NgModule } from '@angular/core';
 import { LandPageComponent } from './land-page/land-page.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, FooterComponent, LandPageComponent],
+  declarations: [AppComponent, MenuComponent, FooterComponent, LandPageComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
