03d36ef670d5665e9c54216cbd8de7f3a14d9ee2
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2021-07-22 21:56:59 -0300

    Adicionado ngstyle

--- src/app/land-page/land-page.component.ts
+++ src/app/land-page/land-page.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-land-page',
   templateUrl: './land-page.component.html',
-  styleUrls: ['./land-page.component.css']
+  styleUrls: ['./land-page.component.css'],
 })
 export class LandPageComponent implements OnInit {
+  imageURL: string = 'https://lorempixel.com/600/300/business/';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getBackgroundImage() {
+    return {
+      'background-image':
+        'linear-gradient(rgba(0, 0, 0, .7), rgba(0, 0, 0, .9)), url(' +
+        this.imageURL +
+        ')',
+    };
   }
 
+  ngOnInit(): void {}
 }
