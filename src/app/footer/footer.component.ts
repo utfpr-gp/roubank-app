e48e7f19f0297885323c0c7f25974bd8b1968425
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2021-07-22 22:45:03 -0300

    Adicionado interpolação

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  year: number;
+  constructor() {
+    this.year = new Date().getFullYear();
   }
 
+  ngOnInit(): void {}
 }
